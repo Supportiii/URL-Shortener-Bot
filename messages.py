9510274ec90b52db44fe8592ee048e8c552e2daa
Supporti <89602269+Supportiii@users.noreply.github.com>
2021-09-09 22:20:54 +0200

    Update messages.py

--- messages.py
+++ messages.py
@@ -1,27 +1,31 @@
-IT = "it"
 EN = "en"
 DE = "de"
+IT = "it"
 
 WELCOME_MESSAGE = {
                         EN : "Send a link at any time that you want to shorten with bit.ly\n",
-                        IT : " Invia in qualsiasi momento un link che vuoi accorciare con bit.ly\n",
-                        DE : " Sende nun den Link, den du kürzen möchtest."
+                        DE : " Sende nun den Link, den du kürzen möchtest.\n",
+                        IT : " Invia in qualsiasi momento un link che vuoi accorciare con bit.ly"
   
                     }
 HELP_MESSAGE = {
                         EN : "This bot allows you to short a link with the bit.ly service. Send a valid link at any time to get the shortened link. You can also use the inline mode writing {} (followed by the link you want to shrink) in any chat!\n\n",
+                        DE : "Dieser Bot ermöglicht es, Links mit bit.ly zu kürzen. Sende einen gültigen Link und ich werde ihn für Dich kürzen. Du kannst auch mit {} den Inline Modus nutzen. (gefolgt von dem Link, den du kürzen möchtest) in jedem Chat!\n\n",
                         IT : "Questo bot ti permette di accorciare un link con il servizio bit.ly. Manda un link valido in qualsiasi momento per ottenere il link accorciato. Puoi anche usare la inline mode scrivendo {} (seguito dal link che vuoi accorciare) in qualsiasi chat!"
                     }
 INVALID_URL_MESSAGE = {
                         EN : "The link you inserted is invalid.\n",
+                        EN : "Dieser Link ist ungültig.\n",
                         IT : "Il link che hai inserito non è valido."
                     }
 GENERIC_ERROR_MESSAGE = {
                         EN : "Something went wrong...\n",
+                        EN : "3RR0R...\n",
                         IT : "Qualcosa è andato storto..."
                         }
 
 INLINE_TITLE = {
                         EN : "Send shortened link",
+                        EN : "Gekürzten Link senden",
                         IT : "Invia link accorciato"
                 }
